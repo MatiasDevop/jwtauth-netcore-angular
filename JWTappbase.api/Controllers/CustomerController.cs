22e54be6b3ea856dd6225989960a118a44c551af
MatiasDevop <solomatias321@gmail.com>
2021-02-18 17:46:49 +0100

    building jwt auth api and connnect to angular

---
+++ JWTappbase.api/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTappbase.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IEnumerable<string> Index()
+        {
+            return new string[] { "jhon doe", "sarah connor" };
+        }
+    }
+}
